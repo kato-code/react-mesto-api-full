82c00f8ca7c0e3bc97f438b66c0607da99582d05
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2021-03-26 13:08:20 +0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,24 @@
 /* eslint-disable no-console */
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+// const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 
 const { createUser, loginUser } = require('./controllers/users.js');
-const auth = require('./middlewares/auth.js');
 const { requestLogger, errorLogger } = require('./middlewares/logger.js');
 const { signinValidator, signupValidator } = require('./middlewares/validators.js');
+const auth = require('./middlewares/auth.js');
 
 const routesUsers = require('./routes/users.js');
 const routesCards = require('./routes/cards.js');
 const routeNotFound = require('./routes/routeNotFound.js');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 // cors
-app.use(cors());
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -29,8 +27,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 })
   .then(() => console.log('Connected to DB'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// логгер запросов
+app.use(requestLogger);
 
 // краш тест
 app.get('/crash-test', () => {
@@ -39,15 +42,14 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-// логгер запросов
-app.use(requestLogger);
-
 // обработчики роутов
 app.post('/signin', signinValidator, loginUser);
 app.post('/signup', signupValidator, createUser);
 
-app.use('/', auth, routesUsers);
-app.use('/', auth, routesCards);
+app.use(auth);
+
+app.use('/', routesUsers);
+app.use('/', routesCards);
 app.use('/', routeNotFound);
 
 // логгер ошибок
