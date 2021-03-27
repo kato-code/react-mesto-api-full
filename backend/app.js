869bd85bfc67173a21d031c481c4688872181ada
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2021-03-27 17:46:13 +0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -35,21 +35,6 @@ app.use(express.json());
 // логгер запросов
 app.use(requestLogger);
 
-app.disable('x-powered-by');
-
-app.use((req, res, next) => {
-  res.set({
-    'Access-Control-Allow-Origin': 'https://project.mesto.nomoredomains.icu',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Credentials': true,
-    'Content-Security-Policy': 'default-src "self"; img-src *',
-    'Referrer-Policy': 'no-referrer',
-  });
-
-  next();
-});
-
 // краш тест
 app.get('/crash-test', () => {
   setTimeout(() => {
@@ -84,74 +69,3 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-// /* eslint-disable no-console */
-// const express = require('express');
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// // const bodyParser = require('body-parser');
-// const { errors } = require('celebrate');
-
-// const { createUser, loginUser } = require('./controllers/users.js');
-// const { requestLogger, errorLogger } = require('./middlewares/logger.js');
-// const { signinValidator, signupValidator } = require('./middlewares/validators.js');
-// const auth = require('./middlewares/auth.js');
-
-// const routesUsers = require('./routes/users.js');
-// const routesCards = require('./routes/cards.js');
-// const routeNotFound = require('./routes/routeNotFound.js');
-
-// const { PORT = 3000 } = process.env;
-// const app = express();
-
-// // cors
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true,
-// })
-//   .then(() => console.log('Connected to DB'));
-
-// app.use(cors());
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // логгер запросов
-// app.use(requestLogger);
-
-// // краш тест
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
-
-// // обработчики роутов
-// app.post('/signin', signinValidator, loginUser);
-// app.post('/signup', signupValidator, createUser);
-
-// app.use(auth);
-
-// app.use('/', routesUsers);
-// app.use('/', routesCards);
-// app.use('/', routeNotFound);
-
-// // логгер ошибок
-// app.use(errorLogger);
-
-// // обработчики ошибок
-// app.use(errors());
-
-// // централизованный обработчик
-// app.use((err, req, res, next) => {
-//   res.status(500).send({ message: 'На сервере произошла ошибка' });
-
-//   next();
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`On port ${PORT}`);
-// });
