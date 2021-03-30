0c63b115097c9692802a4c29ed22fed6a584fd29
Katya Ksenofontova <dkurach@MacBook-Air-Dmitry.local>
2021-03-30 07:54:13 +0300

    fix:  createUser

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -57,20 +57,30 @@ const getUserProfileById = (req, res, next) => {
 
 // создать нового юзера
 const createUser = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-      User.create({
-        email: req.body.email,
-        password: hash,
-      })
-        .then((user) => {
-          res.send({ user });
-        })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+
+  User.findOne({ email })
+    .then((data) => {
+      if (data) {
+        throw new ConflictError('Пользователь уже существует');
+      }
+      return bcrypt.hash(password, 10)
+        .then((hash) => User.create({
+          name, about, avatar, email, password: hash,
+        }))
+        .then((user) => res.status(200).send({
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          email: user.email,
+        }))
         .catch((error) => {
           if (error.name === 'ValidationError') {
-            next(new BadRequestError('Введены некорректные данные'));
+            throw new BadRequestError('Введены некорректные данные');
           }
-          next(new ConflictError('Пользователь уже существует'));
+          next(error);
         });
     })
     .catch(next);
